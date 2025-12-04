9a7dda79a8ccece07a16dd165d4004afce5adf32
Yusuf Riduan <yusufriduan@outlook.com>
2025-12-04 11:52:52 +0800

    early revision for the profile-card for the matchmaking

---
+++ src/app/components/profile_card.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+function ProfileCard() {
+  return (
+    <div className="profile-card-wrapper">
+      <div className="profile-card">
+        <div>
+          <h2>USER NAME placeholder</h2>
+          <p>User Description placeholder</p>
+        </div>
+        <div style={{ marginTop: "5em" }}>
+          <p style={{ fontSize: "small" }}>Number of STEM Projects placeholder</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProfileCard;
