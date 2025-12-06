cf789adab80546e37aa375d1fc5f57aaab5a20a2
Shawn Huang Qi Yang <htp8090@gmail.com>
2025-12-06 15:41:12 +0800

    Login and sign in db connection

---
+++ src/app/logout/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+
+export default function logout() {
+  const router = useRouter();
+  useEffect(() => {
+    if (Cookies.get("User")) {
+      Cookies.remove("User");
+    }
+    router.push("/login");
+  }, [router]);
+
+  return null;
+}
